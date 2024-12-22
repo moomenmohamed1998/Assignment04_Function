34e8779ce3b786ac061a2045f15b2efb3225da41
moomen mohammmed <monm73530@gmail.com>
2024-12-22 10:51:05 +0200

    Add project files.

---
+++ Assignment04_Function/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment04_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
