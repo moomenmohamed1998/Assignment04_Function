20bcb75302e55a3195fdb895131396d5aa05b924
moomen mohammmed <monm73530@gmail.com>
2024-12-24 23:59:53 +0200

    Demo

--- Assignment04_Function/Program.cs
+++ Assignment04_Function/Program.cs
@@ -7,10 +7,60 @@ namespace Assignment04_Function
     internal class Program
     {
 
+        #region 3-Write a c# Function that accept 4 parameters from user and return result of summation and subtracting of two numbers
+
+        public static void sumsub(int num01, int num02, int num03, int num04, out int sum, out int sub)
+        {
+             sum = num01 + num02;
+             sub = num01 - num02;
+
+            //return new { sub, sum };
+
+        }
+
+        #endregion
+        
+        #region 4-Write a program in C# Sharp
+
+        public static int Sum(ref int[] arr)
+        {
+            int sum = 0;
+
+            if (arr != null)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                    sum += arr[i];
+            }
+            return sum;
+
+        }
+
+        #endregion
+
+        #region 5-Create a function named "IsPrime"
+
+        public static object sPrime(int Num)
+        {
+            int i = 2;
+
+            if (Num % i == 0)
+            {
+                
+                return "true";
+
+            }
+            else
+            {
+                return "false";
+            }
+
+        }
+        #endregion
+
         #region 7-Create an iterative (non-recursive) function to calculate the factorial of the number specified as parameter
         public static void nonrecursive(int Num)
         {
-          for(int i = 1; i <= Num; i++)
+            for (int i = 1; i <= Num; i++)
             {
                 Console.WriteLine($"{Num}");
 
@@ -24,7 +74,7 @@ namespace Assignment04_Function
             string temp = x;
             x = y;
             y = temp;
-        } 
+        }
         #endregion
 
 
@@ -50,9 +100,40 @@ namespace Assignment04_Function
 
             #endregion
 
+            #region 3-Write a c# Function that accept 4 parameters from user and return result of summation and subtracting of two numbers
+
+            //int A = 10;
+            //int B = 3;
+            //int sum, sub;
+
+            //sumsub(A, B,A,B, out sum,out sub);
+
+            //Console.WriteLine($"sum = {sum}");
+            //Console.WriteLine($"sub = {sub}");
+
+            #endregion
+
+            #region 4-Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number.
+            //int[] number = { 3,3 };
+
+            //int result = Sum(ref number); 
+
+            //Console.WriteLine($"The sum of the digits of the {number[0]}{number[1]} is: {result}");
+
+            #endregion
+
+            #region 5-Create a function named "IsPrime", which receives an integer number and retuns true if it is prime, or false if it is not:
+            //sPrime(5);
+            #endregion
+
+            #region 6-Create a function named MinMaxArray, to return the minimum and maximum values stored in an array, using reference parameters
+
+
+
+            #endregion
 
             #region 7-Create an iterative (non-recursive) function to calculate the factorial of the number specified as parameter
-            nonrecursive(1);
+            //nonrecursive(1);
             #endregion
 
             #region 8-Create a function named "ChangeChar" to modify a letter in a certain position (0 based) of a string, replacing it with a different letter
